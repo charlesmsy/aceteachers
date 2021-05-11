8deae96c9d3fa12be27bb674aeac847d28838b46
Charles Sy <50850323+charlesmsy@users.noreply.github.com>
2021-05-11 00:00:08 -0700

    Initial Commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_nWxqHNVO6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4cfvfk53bpthvslpq69ea67s11', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://tktnb50eh9.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
